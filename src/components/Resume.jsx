658dcf64ea1ad1a65b36cc4e9421baef9092d9f4
Pedro Gutierrez <pedrogutierreznavarro@gmail.com>
2023-03-10 14:12:15 -0600

    Add resume

---
+++ src/components/Resume.jsx
@@ -0,0 +1,11 @@
+import './Resume.styles.css';
+
+const Resume = () => {
+  return(
+    <div className="resume">
+      <a title="Download Resume" className="btn btn-outline-light btn-lg" href="/resume-pedro-gutierrez.pdf" target="_blank">Download Resume</a>
+    </div>
+  )
+};
+
+export default Resume;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,15 @@ import Experience from "../components/Experience"
 import Contact from "../components/Contact"
 import Header from "../components/Header/Header"
 import Section from '../components/Section';
+import Resume from '../components/Resume';
 
 const Home = () => {
   return (
     <div >
       <Header/>
+      <Section>
+        <Resume/>
+      </Section>
       <Section title="Skills">
         <Skills/>
       </Section>
