cadee514661b69cf5bdcc2105b9fbbf04cb73e43
pdro-egn <pedrogutierreznavarro@gmail.com>
2023-04-17 09:52:17 -0700

    Add images to projects

--- src/features/project-details/Projects.jsx
+++ src/features/project-details/Projects.jsx
@@ -6,7 +6,7 @@ import StepLabel from '@mui/material/StepLabel';
 import StepContent from '@mui/material/StepContent';
 import Typography from '@mui/material/Typography';
 
-import { Stack, useTheme } from '@mui/material';
+import { Stack, useTheme, Chip } from '@mui/material';
 import { RocketLaunch } from '@mui/icons-material';
 import skills from '../../data/skills.json';
 
@@ -41,49 +41,79 @@ const Projects = ({ projectList }) => {
                 </Typography>
             </StepLabel>
             <StepContent>
+                {p?.images ? (
+                    <Stack direction="row" gap={2} mb={2} sx={{
+                        maxWidth: '100%',
+                        overflowX: 'scroll',
+                        padding: 1,
+                    }}>
+                        {p.images.map((image, index) => (
+                            <Box
+                                sx={{
+                                    backgroundColor: 'white',
+                                    padding: 1,
+                                    borderRadius: 2,
+                                    boxShadow: '0px 1px 2px rgba(0,0,0,0.25)',
+                                    cursor: 'pointer'
+                                }}
+                            >
+                                <Box
+                                    sx={{
+                                        width: 150,
+                                        height: 150,
+                                        background: `url(/projects/on-frontiers/${image})`,
+                                        backgroundColor: theme.palette.grey[200],
+                                        backgroundSize: 'cover',
+                                        boxShadow: 'inset 0px 0px 4px rgba(0,0,0,0.125)'
+                                    }}
+                                />
+                            </Box>
+                        ))}
+                    </Stack>
+                ) : null}
                 {p?.description ? (
-                    <>
+                    <Box mb={2}>
                         <Typography variant="caption" color="grey">Description:</Typography>
                         <Typography variant="body1">{p.description}</Typography>
-                    </>
+                    </Box>
                 ) : null}
                 {p?.majorContribution ? (
-                    <>
+                    <Box mb={2}>
                         <Typography variant="caption" color="grey">Major Contribution:</Typography>
                         <Typography variant="body1">{p.majorContribution}</Typography>
-                    </>
+                    </Box>
                 ) : null}
                 {p?.mainTasks ? (
-                    <>
+                    <Box mb={2}>
                         <Typography variant="caption" color="grey">Main Tasks:</Typography>
                         <ul>
                             {p.mainTasks?.map((t, i) => (
                                 <li key={`task-${i}`}>{t}</li>
                             ))}
                         </ul>
-                    </>
+                    </Box>
                 ) : null}
                 {p?.technologies ? (
                     <>
                         <Typography variant="caption" color="grey">Stack:</Typography>
                         <br/>
-                        {p.technologies.map((t, index) => {
+                        <Stack gap={1} flexDirection="row">
+                            {p.technologies.map((t, index) => {
                                 if (skills[t]) {   
-                                    return(
-                                        <span
-                                            key={`technology-${index}`}
-                                            className="badge rounded-pill me-2 py-2 px-3"
-                                            data-bs-toggle="tooltip"
-                                            data-bs-placement="top"
-                                            title={`${skills[t].years} years of experience and counting...`}
-                                            style={{ backgroundColor: skills[t].color }}
-                                        >
-                                            {skills[t].name} {skills[t].years}x
-                                        </span>
+                                    return (
+                                            <Chip
+                                                key={`technology-${index}`}
+                                                label={`${skills[t].name} ${skills[t].years}x`}
+                                                sx={{
+                                                    color: 'white',
+                                                    backgroundColor: skills[t].color
+                                                }}
+                                            />
                                     )
                                 }
                                 return null;
                             })}
+                        </Stack>
                     </>
                 ) : null}
             </StepContent>
